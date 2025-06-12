ec0e45e85456665e0589339c4b7d6c4dbc941d9a
NorbertoZirio <norbertozirio@gmail.com>
2025-06-12 16:47:46 -0700

    create files an make the communication
    also create the greed

---
+++ script.js
@@ -0,0 +1,9 @@
+const container=document.querySelector("#greedContainer")
+
+for (i=0;i<16*16;i++){
+    const content=document.createElement("div")
+    content.classList.add("greenSquare")
+    
+
+    greedContainer.appendChild(content)
+}
